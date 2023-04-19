8ded3317043d0851311a14b88f73c5bd3f759a97
LukeKirkby <luke.kirkby@icrar.org>
2023-04-19 17:46:03 +0800

    Update functions.py

--- sourcecode/functions.py
+++ sourcecode/functions.py
@@ -1,2 +1,22 @@
-def load_image():
-    
+def load_image(self):
+     # Open a file selection dialog box to choose an image file
+        file_path = filedialog.askopenfilename(title="Select Image File", filetypes=[("Image Files", "*.png;*.jpg;*.jpeg;*.bmp;*.gif")])
+        
+        # Load the chosen image using PIL
+        self.original_image = Image.open(file_path)
+        
+        # Resize the image to fit in the label
+        width, height = self.original_image.size
+        max_size = 250
+        if width > height:
+            new_width = max_size
+            new_height = int(height * (max_size / width))
+        else:
+            new_width = int(width * (max_size / height))
+            new_height = max_size
+        self.original_image = self.original_image.resize((new_width, new_height))
+        
+        # Convert the image to Tkinter format and display it on the left side
+        photo = ImageTk.PhotoImage(self.original_image)
+        self.image_label.configure(image=photo)
+        self.image_label.image = photo
