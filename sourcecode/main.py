4f2ff741a84f7e21c25d54b49a7e8e7a29a7760d
LukeKirkby <luke.kirkby@icrar.org>
2023-05-03 18:22:36 +0800

    Displays 3 Images
    Adds functions to display rgb & grey images in a given location.

--- sourcecode/main.py
+++ sourcecode/main.py
@@ -29,7 +29,6 @@ class MainWindow(QtWidgets.QWidget):
             # Opens image selection window and extracts file path.
             selected_file = QtWidgets.QFileDialog.getOpenFileName(self, "Select Image", os.path.dirname(os.path.realpath(__file__)), "Image File (*.jpeg *.jpg *.jpe *.jp2 *.png)")
             file_path = selected_file[0]
-            #file_path = filedialog.askopenfilename(title="Select Image File", filetypes=[("Image Files", "*.png;*.jpg;*.jpeg;*.bmp;*.gif")])
             print(file_path)
 
             # Checks if an image was actually selected.
@@ -52,12 +51,7 @@ class MainWindow(QtWidgets.QWidget):
                 self.image = cv2.resize(self.image, dim, interpolation = cv2.INTER_AREA)
                 self.colourimage = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
 
-                h, w, ch = self.colourimage.shape
-                qimage = QImage(self.colourimage.data, w, h, ch * w, QImage.Format.Format_RGB888)
-        
-                # Displays original colour image in left panel.
-                self.pixmap = QPixmap.fromImage(qimage)
-                self.original_image.setPixmap(self.pixmap)
+                self.display_colour(self.colourimage,self.original_image)
 
                 # Converts cv2 image from the BGR to the HSV colour space for further processing.
                 self.hsvimage = cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV)
@@ -67,17 +61,12 @@ class MainWindow(QtWidgets.QWidget):
                 green = self.colourSegmentation("green")
                 combined = blue + red + green
 
-                # Converts cv2 image to QImage for display.
-                im_np = np.array(combined)  
-                qimage = QImage(im_np.data, im_np.shape[1], im_np.shape[0], combined.strides[0], QImage.Format.Format_Grayscale8)
-
-                # Displays processed image in the right panel.
-                self.pixmap = QPixmap.fromImage(qimage)
-                self.processed_image.setPixmap(self.pixmap)
+                self.display_greyscale(combined, self.processed_image)
 
                 blue_boxes = self.object_analysis(blue)
                 red_boxes = self.object_analysis(red)
                 green_boxes = self.object_analysis(green)
+
                 for i in range(len(red_boxes)):
                     boxes = cv2.rectangle(self.image, (red_boxes[i][0],red_boxes[i][1]), (red_boxes[i][0] + red_boxes[i][2], red_boxes[i][1] - red_boxes[i][3]), [255, 0,0])
                 for i in range(len(blue_boxes)):
@@ -85,12 +74,8 @@ class MainWindow(QtWidgets.QWidget):
                 for i in range(len(green_boxes)):
                     boxes = cv2.rectangle(self.image, (green_boxes[i][0],green_boxes[i][1]), (green_boxes[i][0] + green_boxes[i][2], green_boxes[i][1] - green_boxes[i][3]), [0, 255,0])
 
-                im_np = np.array(boxes)  
-                qimage = QImage(im_np.data, im_np.shape[1], im_np.shape[0], boxes.strides[0], QImage.Format.Format_RGB888)
-
-                # Displays processed image in the right panel.
-                self.pixmap = QPixmap.fromImage(qimage)
-                self.processed_image.setPixmap(self.pixmap)
+                self.display_colour(boxes, self.render_image)
+                
 
 
     def colourSegmentation(self, request): 
@@ -114,13 +99,7 @@ class MainWindow(QtWidgets.QWidget):
             segmented_image = cv2.cvtColor(segmented_image, cv2.COLOR_BGR2GRAY)
             ret, segmented_image = cv2.threshold(segmented_image, 0, 255, cv2.THRESH_BINARY)
 
-            # Converts cv2 image to QImage for display.
-            im_np = np.array(segmented_image)  
-            qimage = QImage(im_np.data, im_np.shape[1], im_np.shape[0], segmented_image.strides[0], QImage.Format.Format_Grayscale8)
-
-            # Displays processed image in the right panel.
-            self.pixmap = QPixmap.fromImage(qimage)
-            self.processed_image.setPixmap(self.pixmap)
+            self.display_greyscale(segmented_image, self.processed_image)
 
             # Sets flag to indicate that image has been processed.
             self.image_processed = 1
@@ -129,24 +108,15 @@ class MainWindow(QtWidgets.QWidget):
 
     def object_analysis(self, image):
         output = cv2.connectedComponentsWithStats(image, cv2.CV_32S)
+
         (numLabels, labels, stats, centroids) = output
+
         self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
         viable = []
-        for i in range(1, numLabels):
-            # if this is the first component then we examine the
-            # *background* (typically we would just ignore this
-            # component in our loop)
-            if i == 0:
-                text = "examining component {}/{} (background)".format(
-                    i + 1, numLabels)
-            # otherwise, we are examining an actual connected component
-            else:
-                text = "examining component {}/{}".format( i + 1, numLabels)
-            # print a status message update for the current connected
-            # component
-            print("[INFO] {}".format(text))
-            # extract the connected component statistics and centroid for
-            # the current label
+
+        for i in range(1, numLabels): #starts at 1 to exlude the background
+            # Extract the connected component statistics and centroid for the current label
+            # Centroid info currently unused
             x = stats[i, cv2.CC_STAT_LEFT]
             y = stats[i, cv2.CC_STAT_TOP]
             w = stats[i, cv2.CC_STAT_WIDTH]
@@ -157,6 +127,22 @@ class MainWindow(QtWidgets.QWidget):
                 viable.append([x,y,w,h])
 
         return(viable)
+
+    def display_colour(self, image, location):
+        im_np = np.array(image)  
+        qimage = QImage(im_np.data, im_np.shape[1], im_np.shape[0], image.strides[0], QImage.Format.Format_RGB888)
+
+        # Displays processed image in the right panel.
+        self.pixmap = QPixmap.fromImage(qimage)
+        location.setPixmap(self.pixmap)
+
+    def display_greyscale(self, image, location):
+        im_np = np.array(image)  
+        qimage = QImage(im_np.data, im_np.shape[1], im_np.shape[0], image.strides[0], QImage.Format.Format_Grayscale8)
+
+        # Displays processed image in the right panel.
+        self.pixmap = QPixmap.fromImage(qimage)
+        location.setPixmap(self.pixmap)
 
 if __name__ == '__main__':
     uiname = "gui.ui"
